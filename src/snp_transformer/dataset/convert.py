a418095249a69f7e3a0d243046f83d2b3bba66b7
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2023-09-27 12:56:31 +0200

    fix: Added psparse format

---
+++ src/snp_transformer/dataset/convert.py
@@ -0,0 +1,80 @@
+import json
+import logging
+import shutil
+from os import sep
+from pathlib import Path
+
+import polars as pl
+
+logger = logging.getLogger(__name__)
+
+
+def sparse_to_psparse(
+    sparse_path: Path,
+    psparse_path: Path,
+    overwrite: bool = False,
+    batch_size=50_000,
+) -> None:
+    """
+    loads in a sparse file and converts it to a psparse format. A psparse format is a
+    partioned sparse format, where each individual has their own file named after their
+    individual ID within a folder with the .psparse extension. The individual files are
+    .sparse files.
+
+    This function assumed that the sparse file is sorted by individual ID
+
+    Args:
+        sparse_path: path to the sparse file
+        psparse_path: path to the psparse file
+        overwrite: whether to overwrite the psparse file if it already exists. Defaults to False.
+    """
+
+    assert (
+        sparse_path.exists()
+        and sparse_path.is_file()
+        and sparse_path.suffix == ".sparse"
+    )
+
+    # check that the psparse_path does not exist
+    if psparse_path.exists():
+        if overwrite:
+            logger.warning(f"{psparse_path} already exists, overwriting...")
+            shutil.rmtree(psparse_path)
+        else:
+            raise ValueError(
+                f"{psparse_path} already exists, set overwrite=True to overwrite it"
+            )
+
+    psparse_path.with_suffix(".psparse").mkdir(exist_ok=True, parents=True)
+
+    seen_groups = set()
+    reader = pl.read_csv_batched(
+        sparse_path,
+        batch_size=batch_size,
+        separator=" ",
+    )
+
+    batch = reader.next_batches(1)
+
+    while batch is not None:
+        df = batch[0]
+        iids = df.partition_by("Individual", as_dict=True)
+
+        for iid, df in iids.items():
+            path = psparse_path / f"{iid}.sparse"
+            str_iid = str(iid)
+            if str_iid in seen_groups:
+                with path.open(mode="ab") as f:
+                    df.write_csv(f, has_header=False, separator=" ")  # type: ignore
+            else:
+                df.write_csv(path, separator=" ")
+                seen_groups.add(str_iid)
+
+        batch = reader.next_batches(1)
+
+    # save the seen groups to a json file
+    seen_groups_path = psparse_path / "individuals.json"
+    with seen_groups_path.open("w") as f:
+        json.dump(list(seen_groups), f)
+
+    logger.info(f"Saved psparse file to {psparse_path}")
---
+++ tests/test_dataset_loaders.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from snp_transformer.dataset.loaders import Psparse
+
+
+def test_Psparse(test_data_folder: Path):
+    psparse = Psparse(test_data_folder / "data.psparse")
+    assert len(psparse) == 3
+    for ind in psparse.individuals:
+        df = psparse[ind]
