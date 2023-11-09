381eb6e289a63a4a5a82a8eca4384ba4e98ca0eb
Your Name <you@example.com>
2023-11-09 17:41:32 +0100

    debugging

--- src/projects/pretrain_debug/pretrain.py
+++ src/projects/pretrain_debug/pretrain.py
@@ -1,11 +1,12 @@
 import logging
 from pathlib import Path
-
+import os
 from snp_transformer.train import train
 
 logging.basicConfig(level=logging.INFO)
 path = Path(__file__).parent / "config.cfg"
 import torch
-
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 torch.arange(0, 32, 2, device=torch.device("cuda"))
+print("created obj on cuda")
 train(path)
