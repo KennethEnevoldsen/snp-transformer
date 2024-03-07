a7eddbe8628f97191874b86fed474c1170d5f7d1
KennethEnevoldsen_grundtvig <KennethcEnevoldsen@gmail.com>
2024-03-07 11:29:23 +0100

    experimenting on multiple phenotypes

--- src/projects/100-snps/train511_with_big-batch.py
+++ src/projects/100-snps/train511_with_big-batch.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 from snp_transformer.runner import run_from_config_path
 
-# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 # Paths to run
-path = Path(__file__).parent / "fine_tune_no_pretrain_only_100_learned_pos_encoding_non_sparse_big_batch.cfg"
+path = Path(__file__).parent / "fine_tune_no_pretrain_only_511_learned_pos_encoding_big_batch.cfg"
 run_from_config_path(path)
